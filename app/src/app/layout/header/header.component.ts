f30dd8542de27084c137bdea37e3fb067b366ae2
Daniel <daniel.wenglorz92@gmail.com>
2024-03-05 17:48:38 +0100

    Created sketch for header component

--- app/src/app/layout/header/header.component.ts
+++ app/src/app/layout/header/header.component.ts
@@ -1,10 +1,13 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+import {MatTabsModule} from '@angular/material/tabs'
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatIcon, MatIconButton, MatTabsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
