a46d930fd43194e6f4a5643a51224ba3ef2de015
Daniel <daniel.wenglorz92@gmail.com>
2024-03-06 17:24:33 +0100

    Added styles for campaigns tab and created route for it

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,4 +1,7 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {CampaignsComponent} from "./modules/campaigns/campaigns.component";
 
 export const routes: Routes = [
+  // { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: 'campaigns', component: CampaignsComponent},
 ];
--- app/src/app/layout/header/header.component.ts
+++ app/src/app/layout/header/header.component.ts
@@ -3,11 +3,12 @@ import {MatToolbarModule} from '@angular/material/toolbar'
 import {MatIcon} from "@angular/material/icon";
 import {MatIconButton} from "@angular/material/button";
 import {MatTabsModule} from '@angular/material/tabs'
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatIcon, MatIconButton, MatTabsModule],
+  imports: [MatToolbarModule, MatIcon, MatIconButton, MatTabsModule, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
