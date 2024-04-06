ac6cfeafa44f16463337e6628705a693a5003a30
Daniel <daniel.wenglorz92@gmail.com>
2024-04-06 16:55:55 +0200

    Added stepper for create character component

--- app/src/app/modules/players-characters/create-character/create-character.component.ts
+++ app/src/app/modules/players-characters/create-character/create-character.component.ts
@@ -1,13 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {MatStepperModule} from "@angular/material/stepper";
+import {MatButton} from "@angular/material/button";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatRadioModule} from "@angular/material/radio";
 
 @Component({
   selector: 'app-create-character',
   standalone: true,
-  imports: [],
+  imports: [MatStepperModule, MatButton, ReactiveFormsModule, MatFormFieldModule, MatInput, MatRadioModule],
   templateUrl: './create-character.component.html',
   styleUrl: './create-character.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CreateCharacterComponent {
+
+  #formBuilder = inject(FormBuilder);
+
+  nameCharacterForm = this.#formBuilder.group({
+    name: ['', Validators.required],
+    gender: ['', Validators.required],
+    // race: ['', Validators.required],
+  });
+
 
 }
--- app/src/app/modules/players-characters/interfaces/player-character.ts
+++ app/src/app/modules/players-characters/interfaces/player-character.ts
@@ -3,6 +3,7 @@ import {Attributes} from "./attributes";
 export interface PlayerCharacter {
   _id: string;
   name: string;
+  gender: string;
   age: number;
   race: string;
   class: string;
