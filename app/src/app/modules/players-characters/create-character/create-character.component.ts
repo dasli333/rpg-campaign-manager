8c015c127864ef48f44351ec9bfe1944470e3972
Daniel <daniel.wenglorz92@gmail.com>
2024-04-07 17:45:11 +0200

    Changes in create form

--- app/src/app/modules/players-characters/create-character/create-character.component.ts
+++ app/src/app/modules/players-characters/create-character/create-character.component.ts
@@ -5,11 +5,14 @@ import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatRadioModule} from "@angular/material/radio";
+import {MatSelectModule} from "@angular/material/select";
+import {RACES_PL} from "../../../../dnd-data/race/race_pl";
+import {Race} from "../../../../dnd-data/race/race";
 
 @Component({
   selector: 'app-create-character',
   standalone: true,
-  imports: [MatStepperModule, MatButton, ReactiveFormsModule, MatFormFieldModule, MatInput, MatRadioModule],
+  imports: [MatStepperModule, MatButton, ReactiveFormsModule, MatFormFieldModule, MatInput, MatRadioModule, MatSelectModule],
   templateUrl: './create-character.component.html',
   styleUrl: './create-character.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -18,11 +21,33 @@ export class CreateCharacterComponent {
 
   #formBuilder = inject(FormBuilder);
 
+  races = RACES_PL;
+
+  selectedRace: Race | undefined;
+  mainRace: Race | undefined;
+
+
+  raceCharacterForm = this.#formBuilder.group({
+    race: ['', Validators.required],
+    subrace: [''],
+  });
+
   nameCharacterForm = this.#formBuilder.group({
     name: ['', Validators.required],
     gender: ['', Validators.required],
     // race: ['', Validators.required],
   });
+
+  constructor() {
+    this.raceCharacterForm.get('race')?.valueChanges.subscribe(raceId => {
+      this.selectedRace = this.races.find(race => race.id === raceId);
+      this.mainRace = this.selectedRace;
+    })
+
+    this.raceCharacterForm.get('subrace')?.valueChanges.subscribe(subraceId => {
+      this.selectedRace = this.mainRace?.subrace?.find(race => race.id === subraceId);
+    })
+  }
 
 
 }
