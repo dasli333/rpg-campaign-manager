8db5f64dccb1d48f4bae96586eb0854ca4fcf18e
Daniel <daniel.wenglorz92@gmail.com>
2024-04-09 11:14:12 +0200

    Improved view for first step of create character

--- app/src/app/modules/players-characters/race-details/race-details.component.ts
+++ app/src/app/modules/players-characters/race-details/race-details.component.ts
@@ -1,10 +1,16 @@
 import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {Race, Subrace} from "../../../data-services/models/race";
+import {MatIcon} from "@angular/material/icon";
+import {MatListItem, MatListItemAvatar, MatListModule} from "@angular/material/list";
+import {MatDivider} from "@angular/material/divider";
+import {MatButton} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-race-details',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButton, MatDivider, MatListModule, MatListItem, MatIcon, NgIf, NgForOf],
   templateUrl: './race-details.component.html',
   styleUrl: './race-details.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
