c2ead241e037316374b3aef89db89fadeb196562
Daniel <daniel.wenglorz92@gmail.com>
2024-05-27 18:20:31 +0200

    Removed duplicated Http provider

--- app/src/app/app.config.ts
+++ app/src/app/app.config.ts
@@ -3,9 +3,14 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from "@angular/common/http";
-import { graphqlProvider } from './graphql.provider';
+import {provideHttpClient} from "@angular/common/http";
+import {graphqlProvider} from './graphql.provider';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideAnimationsAsync(), provideHttpClient(), provideHttpClient(), graphqlProvider]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    graphqlProvider]
 };
