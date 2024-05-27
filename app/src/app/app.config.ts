a9dd0ec40fb284f6dedff00194e380773ed2c45b
Daniel <daniel.wenglorz92@gmail.com>
2024-05-27 18:16:57 +0200

    Update to Angular 18

--- app/src/app/app.config.ts
+++ app/src/app/app.config.ts
@@ -3,7 +3,7 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {provideHttpClient} from "@angular/common/http";
+import { provideHttpClient } from "@angular/common/http";
 import { graphqlProvider } from './graphql.provider';
 
 export const appConfig: ApplicationConfig = {
--- app/src/app/http/http.service.ts
+++ app/src/app/http/http.service.ts
@@ -1,5 +1,5 @@
 import {inject, Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import {API_URL} from "../../config";
 
 @Injectable({
